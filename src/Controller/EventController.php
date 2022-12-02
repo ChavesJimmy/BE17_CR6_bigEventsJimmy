1b3ee74c3dd7826c23384c08df87d6aa58577809
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-12-02 14:30:25 +0100

    startes styling

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/event')]
 class EventController extends AbstractController
 {
     #[Route('/', name: 'app_event_index', methods: ['GET'])]
